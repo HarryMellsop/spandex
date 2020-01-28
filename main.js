b73b7fc9c2440a0a37b6c000b2983fd254adb7b9
Harry Mellsop <harrymellsop@gmail.com>
2020-01-27 22:44:37 -0800

    Main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let win
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 800,
+    width: 850,
     height: 600,
     webPreferences: {
       nodeIntegration: true
