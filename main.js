a67ee8e523f0ea09b87edea8f9933fca52182625
Harry Mellsop <harrymellsop@gmail.com>
2020-01-27 22:48:33 -0800

    Electron is booting

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ function createWindow () {
   // and load the index.html of the app.
   win.loadFile('index.html')
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+  // Open the DevTools (if required)
+  //win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
